0c46ec18bd3eddcdf4a4ee69c579c5912fbfcc6a
Gagan Kumar S <gagankumars7259@gmail.com>
2025-03-25 12:34:23 +0530

    HTML

--- Js/main.js
+++ Js/main.js
@@ -16,3 +16,23 @@ const closeNav = () => {
 
 openNavBtn.addEventListener("click", openNav);
 closeNavBtn.addEventListener("click", closeNav);
+
+// MANAGE CATEGORIES SIDE BUTTONS FUNCTIONALITY
+
+const sidebar = document.querySelector("aside");
+const showsidebtn = document.querySelector("#show__sidebar-btn");
+const hidesidebtn = document.querySelector("#hide__sidebar-btn");
+
+const showSidebar = () => {
+  sidebar.style.left = "0";
+  showsidebtn.style.display = "none";
+  hidesidebtn.style.display = "inline-block";
+};
+const hideSidebar = () => {
+  sidebar.style.left = "-100%";
+  showsidebtn.style.display = "inline-block";
+  hidesidebtn.style.display = "none";
+};
+
+showsidebtn.addEventListener("click", showSidebar);
+hidesidebtn.addEventListener("click", hideSidebar);
